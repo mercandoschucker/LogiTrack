af270105d12728b0e08f800e89bf42bbc01b2ca2
mercandoschucker <mercandoschucker@gmail.com>
2025-07-05 07:47:45 +0000

    feat: Finish backend project setup and initial run

---
+++ logitrack-backend/src/test/java/com/logitrack/logitrack_backend/LogitrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logitrack.logitrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogitrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
